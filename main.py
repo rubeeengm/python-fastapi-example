89fe3a94accc0d099eee2b55b4326775d7244d51
rubeeengm <rubeeengm@outlook.com>
2022-11-01 22:50:25 -0600

    - validacion de query y path parameters - se agrega documentacion

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Optional
 from pydantic import BaseModel
 
 #FastAPI
-from fastapi import FastAPI, Body, Query
+from fastapi import FastAPI, Body, Query, Path
 
 app = FastAPI()
 
@@ -31,10 +31,33 @@ def create_person(person: Person = Body(...)):
 
 @app.get('/person/detail')
 def show_person(
-    name: Optional[str] = Query(None, min_length = 1, max_length = 50)
-    , age: str = Query(...)
+    name: Optional[str] = Query(
+        None
+        , min_length = 1
+        , max_length = 50
+        , title = 'Person Name'
+        , description = "This is the person name. It's between 1 and 50 characters"
+    )
+    , age: str = Query(
+        ...
+        , title = 'Person Age'
+        , description = "This is the person age. It's required"
+    )
 ):
     return {
         'name': name
         , 'age': age
+    }
+
+@app.get('/person/detail/{person_id}')
+def show_person(
+    person_id: int = Path(
+        ...
+        ,  gt = 0
+        , title = 'Person Id'
+        , description = "This is the person id. It's required and must be more than zero"
+    )
+):
+    return {
+        person_id: 'It exists!'
     }
