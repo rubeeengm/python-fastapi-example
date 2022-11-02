d3f6e0ea628db27a7b3f162b54a47dcd31b13c47
rubeeengm <rubeeengm@outlook.com>
2022-11-01 21:55:01 -0600

    - se agrega nuevo modelo para para procesar request

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
+
+# Models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    hair_color: Optional[str] = None
+    is_married: Optional[bool] = None
 
 @app.get('/')
 def home():
     return {
         'message': 'Hello world'
     }
+
+# Request and Response Body
+
+@app.post('/person/new')
+def create_person(person: Person = Body(...)):
+    return person
